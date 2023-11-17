238adfbbbac3c7dd8188c5f0602921eb5f8d21af
Alejandra Villagomez Flores <micka_seido_19@hotmail.com>
2023-11-17 18:02:06 -0400

    v1

--- Program.cs
+++ Program.cs
@@ -1,31 +1,12 @@
-/*EJERCICIO 1 Encontrar el máximo o el mínimo y mostrarlo en la consola de un array de enteros */
 class Program
 {
-    static void Main()
+    static void Main() //ejercicio 1
     {
-        int[] numeros = { 7, 37, 9, 10, 6, 4, 25 };
-        MostrarMaxMinArrayEnteros(numeros);
-    }
+        int[] numeros = { 5, 2, 8, 1, 9, 4, 3 };
 
-    public static void MostrarMaxMinArrayEnteros(int[] array)
-    {
-        int maximo = array[0];
-        int minimo = array[0];
+        var resultado = ArrayEnteros.EncontrarMaxMin(numeros);
 
-        for (int i = 1; i < array.Length; i++)
-        {
-            if (array[i] > maximo)
-            {
-                maximo = array[i];
-            }
-
-            if (array[i] < minimo)
-            {
-                minimo = array[i];
-            }
-        }
-
-        Console.WriteLine($"El máximo es: {maximo}");
-        Console.WriteLine($"El mínimo es: {minimo}");
+        Console.WriteLine($"Máximo: {resultado.maximo}");
+        Console.WriteLine($"Mínimo: {resultado.minimo}");
     }
 }
