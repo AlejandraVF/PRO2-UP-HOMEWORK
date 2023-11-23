6a8735e96a83f742df602b28d757e174ddf0c55b
Alejandra Villagomez Flores <micka_seido_19@hotmail.com>
2023-11-23 19:06:28 -0400

    v1

---
+++ serie recursiva/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Programacion
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            RecursivoDomino(6);
+        }
+        static int A = 0;
+        static int B = 7;
+        public static void RecursivoDomino(int N)
+        {
+
+            if (N == 0)
+            {
+                return;
+            }
+            else
+            {
+                RecursivoDomino(N - 1);
+                int serieA = A + 1;
+                int serieB = B - 1;
+                A = serieA;
+                B = serieB;
+                Console.WriteLine("A : " + serieA);
+                Console.WriteLine("B : " + serieB);
+
+            }
+        }
+
+    }
+}
